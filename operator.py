50fb3e42b87db77b5831d2cf6ba410623c48102d
Avaneesh verma <124239728+avaneeshverma850@users.noreply.github.com>
2024-01-08 12:06:21 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,31 @@
+#Removing last Digit
+"""print("Enter Number")
+x=int(input())
+y=x//10
+print("Removing Last Digit:",y)"""
+#Print last digit of a given Number
+"""print("Enter number")
+x=int(input())
+x=x%10
+print("Last digit of given number is:",x)"""
+#Swap of two number
+"""print("Enter two numbers")
+x=int(input())
+y=int(input())
+x=x+y
+y=x-y
+x=x-y
+print("After swap x is:",x)
+print("After swap y is:",y)"""
+#Print first digit
+"""print("enter three dgit number")
+x=int(input())
+tem=x//10
+tem=tem//10
+print("First Digit is:",tem)"""
+#printimg last digit of given user
+"""print("Enter 3 digit number")
+x=int(input())
+tem=x//10
+tem=tem%10
+print("Middle digit is:",tem)"""
